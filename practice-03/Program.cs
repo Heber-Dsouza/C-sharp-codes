7646f348ee9e86c8ac99b13049a47e79e21a0dc4
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-14 22:52:41 -0300

    Exercícios de prática 03

---
+++ practice-03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program{
+  static void Main(){
+    char primeiraLetra = 'L';
+    string nome = "Lourenço";
+    int num=0;
+    Console.WriteLine("Meu nome é: " + nome + "...");
+    Console.WriteLine("A primeira letra do meu nome é: " + primeiraLetra);
+    Console.WriteLine("Apenas um número aleatório: " + num);
+    
+//-------------------------------------------------------------------------------
+
+    int num1, num2, result;
+
+    num1=100;
+    num2=15;
+    result= num1 + num2;
+
+    Console.WriteLine("A soma de "+num1 +" + "+num2+" é igual à: "+result);
+    Console.WriteLine("A multiplicação de "+num1 +" com "+num2+" é igual à: "+ num1*num2);
+  }
+}
+
+// byte n1 = 10;
+// int num = -10;
+// char letra = 'L';
+// float valor = 5.4f;
+// string nome = "Lourenço";
+// var aux = -15;
