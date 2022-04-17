8af3f5085654b8691da23428ce4456c9e12cfc36
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-16 23:49:07 -0300

    Exercícios de Prática 09

---
+++ practice-09/ap09.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Aula09 {
+
+  static void Main(){
+
+    int numX = 10;
+    int numY = 20;
+
+    numX <<= 1;
+    numY >>= 1;
+
+    Console.WriteLine(numX);
+    Console.WriteLine(numY);
+  }
+}
+
+// Introdução a operações com bitwise
