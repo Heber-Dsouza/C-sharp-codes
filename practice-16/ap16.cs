2a924ec26ae3f11e44d1a18bbfce084fa2dec0b8
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-18 22:01:03 -0300

    Exercício de Prática 16

---
+++ practice-16/ap16.cs
@@ -0,0 +1,59 @@
+using System;
+
+class Aula16
+{
+
+  static void Main()
+  {
+
+    int tempo;
+    char choose;
+
+    inicio:
+    Console.Clear();
+
+    Console.WriteLine("Belo Horizonte/MG a Vitória/ES");
+    Console.Write("Escolha o transporte: [a]Avião | [c]Carro | [o]Ônibus ");
+    choose = char.Parse(Console.ReadLine());
+
+    switch (choose)
+    {
+
+      case 'a':
+      case 'A':
+        tempo = 50;
+        break;
+      case 'c':
+      case 'C':
+        tempo = 500;
+        break;
+      case 'o':
+      case 'O':
+        tempo = 800;
+        break;
+      default:
+        tempo = -1;
+        break;
+    }
+
+    if (tempo < 0)
+    {
+      Console.WriteLine("Transporte não reconhecido. Digite uma letra válida.");
+    }
+    else
+    {
+      Console.Write("Para o transporte escolhido o tempo é: {0} minutos", tempo);
+    }
+
+    Console.Write("\nCalcular novanente? [s/n]: ");
+    choose = char.Parse(Console.ReadLine());
+
+    if(choose == 's' | choose == 'S'){
+      goto inicio;
+    } else {
+      Console.Clear();
+      Console.WriteLine("Fim do programa...");
+    }
+  }
+
+}
