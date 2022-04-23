5185c3318f3df02768f4db4296b891f19051c438
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-22 22:45:08 -0300

    Exercício de Prática 22

--- practice-22/ap22.cs
+++ practice-22/ap22.cs
@@ -4,7 +4,7 @@ class Aula22 {
 
   static void Main(){
 
-    int[] num = new int[5]{11,22,33,44,55};
+    int[] num = new int[7]{11,22,33,44,55,66,77};
 
     foreach(int n in num){
       Console.WriteLine(n);
